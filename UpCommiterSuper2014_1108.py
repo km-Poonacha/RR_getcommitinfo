79c48bca9768c102799bff6b065436459d82294d
km-poonacha <km.poonacha@gmail.com>
2017-08-14 10:10:01 +0200

    Updated calculation of superpsoition to include commiter information. Further, append with owner information from RepoList2014_new file and also SD_FLAG from the integrated super file

--- UpCommiterSuper2014_1108.py
+++ UpCommiterSuper2014_1108.py
@@ -1,11 +1,64 @@
 from __future__ import division
 import csv
-import time
-from datetime import date
-from datetime import datetime
 
+def addnewrepodata(intsup_row,NEW_REPO_CSV):
+    """Update the repo details with some additional details stores in newRepoList2014 csv file """
+    with open(NEW_REPO_CSV, 'rt',encoding = 'utf-8' ) as New_Repo_read:    
+        New_Repo_analysis = csv.reader(New_Repo_read) 
+        new_index = 0
+        for repo_row in New_Repo_analysis:
+            found_flag = 0
+            if str(intsup_row[0]) == str(repo_row[0]):
+                intsup_row.append(repo_row[19])
+                intsup_row.append(repo_row[20])
+                intsup_row.append(repo_row[21])
+                intsup_row.append(repo_row[22])
+                intsup_row.append(repo_row[23])
+                intsup_row.append(repo_row[24])
+                intsup_row.append(repo_row[25])
+                intsup_row.append(repo_row[26])
+                intsup_row.append(repo_row[27])
+                found_flag = 1
+                return intsup_row
+            else:
+                new_index = new_index + 1   
+                    
+        if found_flag == 0:
+            intsup_row.append('Not Found')
+            intsup_row.append('Not Found')
+            intsup_row.append('Not Found')
+            intsup_row.append('Not Found')
+            intsup_row.append('Not Found')
+            intsup_row.append('Not Found')
+            intsup_row.append('Not Found')
+            intsup_row.append('Not Found')
+            intsup_row.append('Not Found')
+            print ("**********************No match found while searching for new repo deatils")
+            return intsup_row
 
-def create_super(UPDATEDFINAL_CSV,SUPER_CSV):
+def findSDflag(current_repo,INT_SD_CSV):
+    """Include SD flag details into the repository """ 
+    found_flag = 0
+    with open(INT_SD_CSV, 'rt',encoding = 'utf-8' ) as SD_Read:    
+        SD_analysis = csv.reader(SD_Read) 
+        new_index = 0
+        for SD_row in SD_analysis:
+            found_flag = 0
+            if str(current_repo[0]) == str(SD_row[1]):
+                current_repo.append(SD_row[57])
+
+                found_flag = 1
+                return current_repo
+            else:
+                new_index = new_index + 1   
+                    
+        if found_flag == 0:
+            current_repo.append('Not Found')
+            print ("**********************No match found while searching for SD_FLAG")
+            return current_repo
+
+    
+def create_super(UPDATEDFINAL_CSV,SUPER_CSV,NEW_REPO_CSV,INT_SD_CSV):
     
     with open(SUPER_CSV, 'wt',encoding = 'utf-8', newline='' ) as super_append:    
         super_handle = csv.writer(super_append)   
@@ -14,7 +67,7 @@ def create_super(UPDATEDFINAL_CSV,SUPER_CSV):
         pull_no = 0 
         current_repo = []
 # Write the column headings
-        super_handle.writerow(["REPO_ID","NAME","OWNER","OWNER_TYPE","SIZE","CREATE_DATE","PUSHED_DATE","MAIN_LANGUAGE","NO_LANGUAGES","SCRIPT_SIZE","STARS", "WATCHERS", "SUBSCRIPTIONS","OPEN_ISSUES","FORKS", "LICENCE_NAME","URL", "DESCRIPTION","LANG_JSON","NO_CONTRIBUTORS", "NO_PUSHES", "NO_PULLS", "NO_TASKS", "NO_NODES", "DEG_SUPER","AVG_COMMIT"])        
+        super_handle.writerow(["REPO_ID","NAME","OWNER","OWNER_TYPE","SIZE","CREATE_DATE","PUSHED_DATE","MAIN_LANGUAGE","NO_LANGUAGES","SCRIPT_SIZE","STARS", "WATCHERS", "SUBSCRIPTIONS","OPEN_ISSUES","FORKS", "LICENCE_NAME","URL", "DESCRIPTION","LANG_JSON","NO_CONTRIBUTORS", "NO_PUSHES", "NO_PULLS", "NO_TASKS", "NO_NODES", "DEG_SUPER","AVG_COMMITS_PULLREQ","OWNER_PUBLICREPO" ,"OWNER_FOLLOWERS","OWNER_FOLLOWING","OWNER_CREATED","OWNER_HIREABLE","OWNER_EMAIL","TOTAL_CONTRIBUTORS","CONTRIBUTORS_PRE2015","AVG_COMMITS_COMMITTER","SD_FLAG"])        
         
         with open(UPDATEDFINAL_CSV, 'rt',encoding = 'utf-8' ) as new_read:    
             new_analysis = csv.reader(new_read)     
@@ -29,8 +82,10 @@ def create_super(UPDATEDFINAL_CSV,SUPER_CSV):
                 if (row[0] != 'PullRequestEvent') and (row[0] != 'PushEvent'):
 
                     if first_row_flag == 0: 
+                        first_row_flag = first_row_flag+1                        
                         current_repo = row
                         prev_row = row
+                        continue
                     first_row_flag = first_row_flag+1
 #append contributor no                    
                     if prev_row[0] != 'PushEvent' and prev_row[0] != 'PullRequestEvent':
@@ -50,11 +105,29 @@ def create_super(UPDATEDFINAL_CSV,SUPER_CSV):
                         
                         avg_commit = tot_commit / pull_no
                     
-                    print ('Degree of superposition for repo no -', current_repo[0],contributor_no, push_no, pull_no, task_count, node_count, deg_super) 
-                    print ('**Total commit**', tot_commit, 'Avg commit', avg_commit)
-                    print ('')
-                  
-                    super_handle.writerow([current_repo[0],current_repo[1],current_repo[2],current_repo[3],current_repo[4],current_repo[5],current_repo[6],current_repo[7],current_repo[8],current_repo[9],current_repo[10], current_repo[11], current_repo[12],current_repo[13],current_repo[14], current_repo[15], current_repo[16], current_repo[17], current_repo[18],contributor_no, push_no, pull_no, task_count, node_count, deg_super,avg_commit])
+                    #print ('Degree of superposition for repo no -', current_repo[0],contributor_no, push_no, pull_no, task_count, node_count, deg_super) 
+                    #print ('**Total commit**', tot_commit, 'Avg commit', avg_commit)
+                    #print ('')
+                    """Append super details"""
+                    current_repo.append(contributor_no)
+                    current_repo.append(push_no)
+                    current_repo.append(pull_no)
+                    current_repo.append(task_count)
+                    current_repo.append(node_count)
+                    current_repo.append(deg_super)
+                    current_repo.append(avg_commit)
+
+                    """ Get additional repo details from NEW_REPO_CSV"""
+                    current_repo = addnewrepodata(current_repo,NEW_REPO_CSV)
+                    
+                    """To overcoem the problems dealing with the first repo """
+
+                    
+                                        
+                    """ Add SD_FLAG to the super repo """
+                    current_repo = findSDflag(current_repo, INT_SD_CSV)
+                    super_handle.writerow(current_repo)
+                    
                     current_repo = row
                     node_count = 0
                     task_count = 0 
@@ -106,8 +179,10 @@ def create_super(UPDATEDFINAL_CSV,SUPER_CSV):
 def main():
     
     UPDATEDFINAL_CSV = '/Users/medapa/Dropbox/HEC/Data GitHub/2014/Run 5-1/UpdateCommit/UpdateCommitFinal2014V2_24.csv'
-    SUPER_CSV = '/Users/medapa/Dropbox/HEC/Data GitHub/2014/Run 5-1/UpdateCommit/UpCommitSuper2014_24.csv'
-    create_super(UPDATEDFINAL_CSV,SUPER_CSV)
+    SUPER_CSV = '/Users/medapa/Dropbox/HEC/Data GitHub/2014/Run 5-1/UpdateCommit/UpCommitSuper2014_24.csv'    
+    NEW_REPO_CSV = '/Users/medapa/Dropbox/HEC/Data GitHub/2014/Run 5-1/RepoList2014_New.csv'
+    INT_SD_CSV = '/Users/medapa/Dropbox/HEC/Data GitHub/2014/New IntegratedSuper/FULLDATAIntegratedSuper2014_June_UPV2.csv'
+    create_super(UPDATEDFINAL_CSV,SUPER_CSV,NEW_REPO_CSV,INT_SD_CSV)
     
 
 if __name__ == '__main__':
